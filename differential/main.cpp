babfe681aa76c1b9d03001458c74d54faf791d74
Egor Manylov <egormanylov@gmail.com>
2015-09-29 23:01:11 +0600

    Initial Commit

---
+++ differential/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  differential
+//
+//  Created by Егор Манылов on 29.09.15.
+//  Copyright (c) 2015 Egor_Mn. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
